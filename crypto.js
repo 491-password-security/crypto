b025a3d5b7e12ed0fc4e1f0ec0276bf1578f57e7
egeerdogan <ege@erdogan.dev>
2021-11-18 15:10:29 +0300

    separate ot module

--- crypto.js
+++ crypto.js
@@ -6,17 +6,12 @@ const { BigInteger } = require('jsbn');
 const BIG_TWO = new BigInteger('2');
 const BIG_ONE = new BigInteger('1');
 
+const MOD = new BigInteger('104334873255401717971305551311108568981602782554133676271604158174023613565338436519535738349159664075981513545995816898351274759273689547803611869080590323788134546218679576525351375421659491479861062524332418185137628175629882792848502958254366030986728999054034830850220407425928535174607722203029578103539');
+const GEN = new BigInteger('15309569078288033140294527228325069587420150399530450735556668091277116408023136181284430449588830517258893721878398739530623279778683647761572205172467420662396761999763043433000129229039419004108765113420973429371572791200022523422170732284615282345655002021445578558188416639692531759416866286539604862128');
+
 // TODO: reorganize
 // TODO: comments
 // TODO: pseudorandom function
-
-// TODO: is this ok? (I think it is)
-// these are *not* secret values
-// MOD = prime
-// GEN = generator of multiplicative group Z_p
-// GEN ^ (MOD - 1) = 1 mod MOD
-const MOD = new BigInteger('104334873255401717971305551311108568981602782554133676271604158174023613565338436519535738349159664075981513545995816898351274759273689547803611869080590323788134546218679576525351375421659491479861062524332418185137628175629882792848502958254366030986728999054034830850220407425928535174607722203029578103539');
-const GEN = new BigInteger('15309569078288033140294527228325069587420150399530450735556668091277116408023136181284430449588830517258893721878398739530623279778683647761572205172467420662396761999763043433000129229039419004108765113420973429371572791200022523422170732284615282345655002021445578558188416639692531759416866286539604862128');
 
 
 function random(bits, returnBits=false) {
@@ -77,8 +72,6 @@ function newShare(id, shares) {
     return secrets.newShare(id, shares);
 }
 
-module.exports = {random, hash, encrypt, decrypt, share, combine, newShare};
-
 function getBoundedBigInt(max) {
     let bits = max.bitLength();
     do {
@@ -109,81 +102,4 @@ function wordWiseXOR(u, v) {
     return result;
 }
 
-class ObliviousTransferReceiver {
-    constructor(choice) {
-        // TODO: raise exception if choice not one or zero
-        this.choice = choice;
-        this.keys = [];
-        
-        // pick random element from additive Z_p
-        this.k = getBoundedBigInt(MOD);        
-    }
-
-    generateKeys(C) {
-        // generate two random keys (as elements from multiplicative Z_p) also using C
-        let choiceKey = GEN.modPow(this.k, MOD);
-        let negChoiceKey = C.divide(choiceKey).mod(MOD);
-        this.keys = [choiceKey, negChoiceKey];
-    }
-
-    readMessage(choices) {
-        // choose one of the messages
-        let ciphertext = choices[this.choice];
-
-        // g^(r_sigma)^k = PK_sigma^(r_sigma)
-        let xorKey = extendedHash(ciphertext[0].modPow(this.k, MOD), 4);
-
-        // decrypt the ciphertext
-        return wordWiseXOR(ciphertext[1], xorKey);
-    }
-}
-
-class OblivousTransferSender {
-    constructor(m_0, m_1) {
-        this.m_0 = m_0;
-        this.m_1 = m_1;
-
-        // initiate random constants
-        this.C = getBoundedBigInt(MOD);
-        this.r_0 = getBoundedBigInt(MOD);
-        this.r_1 = getBoundedBigInt(MOD);
-    }
-
-    generateKeys(receiverKey) {
-        // generate keys for each message based on receiver's key and the hidden random values
-        this.key_0 = receiverKey.modPow(this.r_0, MOD);
-        this.key_1 = C.divide(this.key_0).modPow(this.r_1, MOD);
-        this.keys = [this.key_0, this.key_1];
-    }
-
-    encryptMessages() {
-        // encrypt (hash + xor) each message using one of the keys
-        let e_0 = [GEN.modPow(this.r_0, MOD), wordWiseXOR(extendedHash(this.key_0, 4), this.m_0)];
-        let e_1 = [GEN.modPow(this.r_1, MOD), wordWiseXOR(extendedHash(this.key_1, 4), this.m_1)];
-        return [e_0, e_1];
-    }
-}
-
-// let m_0 = getBoundedBigInt(MOD);
-// let m_1 = getBoundedBigInt(MOD);
-
-// console.log(m_0);
-// console.log(m_1);
-
-// let receiver = new ObliviousTransferReceiver(0);
-// let sender = new OblivousTransferSender(m_0, m_1);
-
-// let C = sender.C;
-
-// receiver.generateKeys(C);
-
-// let receiverKey = receiver.keys[receiver.choice];
-
-// sender.generateKeys(receiverKey);
-
-// [e_0, e_1] = sender.encryptMessages();
-
-// let result = receiver.readMessage([e_0, e_1]);
-
-// console.log(result);
-
+module.exports = {MOD, GEN, random, hash, encrypt, decrypt, share, combine, newShare, getBoundedBigInt, getElGamalKeys, wordWiseXOR, extendedHash};
---
+++ ot.js
@@ -0,0 +1,59 @@
+var crypto = require('./crypto.js');
+
+const MOD = crypto.MOD;
+const GEN = crypto.GEN;
+
+module.exports.ObliviousTransferReceiver = class ObliviousTransferReceiver {
+    constructor(choice) {
+        // TODO: raise exception if choice not one or zero
+        this.choice = choice;
+        this.keys = [];
+        
+        // pick random element from additive Z_p
+        this.k = crypto.getBoundedBigInt(MOD);        
+    }
+
+    generateKeys(C) {
+        // generate two random keys (as elements from multiplicative Z_p) also using C
+        let choiceKey = GEN.modPow(this.k, MOD);
+        let negChoiceKey = C.divide(choiceKey).mod(MOD);
+        this.keys = [choiceKey, negChoiceKey];
+    }
+
+    readMessage(choices) {
+        // choose one of the messages
+        let ciphertext = choices[this.choice];
+
+        // g^(r_sigma)^k = PK_sigma^(r_sigma)
+        let xorKey = crypto.extendedHash(ciphertext[0].modPow(this.k, MOD), 4);
+
+        // decrypt the ciphertext
+        return crypto.wordWiseXOR(ciphertext[1], xorKey);
+    }
+}
+
+module.exports.ObliviousTransferSender = class ObliviousTransferSender {
+    constructor(m_0, m_1) {
+        this.m_0 = m_0;
+        this.m_1 = m_1;
+
+        // initiate random constants
+        this.C = crypto.getBoundedBigInt(MOD);
+        this.r_0 = crypto.getBoundedBigInt(MOD);
+        this.r_1 = crypto.getBoundedBigInt(MOD);
+    }
+
+    generateKeys(receiverKey) {
+        // generate keys for each message based on receiver's key and the hidden random values
+        this.key_0 = receiverKey.modPow(this.r_0, MOD);
+        this.key_1 = this.C.divide(this.key_0).modPow(this.r_1, MOD);
+        this.keys = [this.key_0, this.key_1];
+    }
+
+    encryptMessages() {
+        // encrypt (hash + xor) each message using one of the keys
+        let e_0 = [GEN.modPow(this.r_0, MOD), crypto.wordWiseXOR(crypto.extendedHash(this.key_0, 4), this.m_0)];
+        let e_1 = [GEN.modPow(this.r_1, MOD), crypto.wordWiseXOR(crypto.extendedHash(this.key_1, 4), this.m_1)];
+        return [e_0, e_1];
+    }
+}
