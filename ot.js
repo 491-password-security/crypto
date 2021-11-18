f2202330ec29c3b8853d0e16ea333c51e3cd1f3a
egeerdogan <ege@erdogan.dev>
2021-11-18 15:40:50 +0300

    ot with network callbacks

--- ot.js
+++ ot.js
@@ -4,13 +4,32 @@ const MOD = crypto.constants.MOD;
 const GEN = crypto.constants.GEN;
 
 module.exports.ObliviousTransferReceiver = class ObliviousTransferReceiver {
-    constructor(choice) {
-        // TODO: raise exception if choice not one or zero
+    constructor(choice, sendCallback, receiveCallback) {
+        if (choice != 0 && choice != 1) {
+            throw new Error('Choice neither 1 nor 0. Enter a single integer (0 or 1) as the choice.');
+        }
+        this.sendCallback = sendCallback;
+        this.receiveCallback = receiveCallback;
         this.choice = choice;
         this.keys = [];
         
         // pick random element from additive Z_p
         this.k = crypto.util.getBoundedBigInt(MOD);        
+    }
+
+    start(address) {
+        // get the random constant C from the sender
+        let C = this.receiveCallback();
+
+        // generate two keys and send the valid key to the sender
+        this.generateKeys(C);
+        this.sendCallback(address, this.keys[this.choice]);
+
+        // receive the two encryptions from the sender
+        let choices = this.receiveCallback();
+
+        // decrypt the chosen message
+        return this.readMessage(choices);
     }
 
     generateKeys(C) {
@@ -33,14 +52,31 @@ module.exports.ObliviousTransferReceiver = class ObliviousTransferReceiver {
 }
 
 module.exports.ObliviousTransferSender = class ObliviousTransferSender {
-    constructor(m_0, m_1) {
+    constructor(m_0, m_1, sendCallback, receiveCallback) {
         this.m_0 = m_0;
         this.m_1 = m_1;
+        this.sendCallback = sendCallback;
+        this.receiveCallback = receiveCallback;
 
         // initiate random constants
         this.C = crypto.util.getBoundedBigInt(MOD);
         this.r_0 = crypto.util.getBoundedBigInt(MOD);
         this.r_1 = crypto.util.getBoundedBigInt(MOD);
+    }
+
+    start(address) {
+        // send the constant value C to the receiver
+        this.sendCallback(address, this.C);
+
+        // receive one key from receiver
+        let receiverKey = this.receiveCallback(address);
+
+        // generate two keys based on the received key and the hidden random values
+        this.generateKeys(receiverKey);
+
+        // send the encrypted messages to the receiver
+        let messages = this.encryptMessages();
+        this.sendCallback(address, messages);
     }
 
     generateKeys(receiverKey) {
